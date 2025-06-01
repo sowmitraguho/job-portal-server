53c45bce58b3308006ff2935a77e721b4eabf6c8
Sowmitra Guha <sowmmochy@gmail.com>
2025-06-01 13:34:52 +0600

    everything make like before

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 //middle wire
-app.use(cors())
-app.use(express.json())
+app.use(cors({
+  origin: 'http://localhost:5173/'
+}));
+app.use(express.json());
 
 //Mongdb connections
 
